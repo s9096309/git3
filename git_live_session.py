ab9022a2b6b2e01b4aecee362afa42ce3ba769ec
s9096309 <k-hoff-mann@web.de>
2025-01-09 12:36:30 +0100

    fixed git ignore?

--- git_live_session.py
+++ git_live_session.py
@@ -1,6 +1,16 @@
 student = {
     "name": "Peter",
-    "grades": []
+    "grades": [1, 5, 2],
     "lastname": "Wurst"
 }
 
+def show_info(student):
+    # Access values from the dictionary using keys
+    print(f"""
+    Name: {student['name']},
+    Grades: {student['grades']},
+    Last Name: {student['lastname']}
+""")
+
+# Call the function
+show_info(student)
